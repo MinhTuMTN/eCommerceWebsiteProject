cc85a9e0ea430042c8b62f6cbc72c0a4a766b4e9
Minh Tu Nguyen <88047847+MinhTuMTN@users.noreply.github.com>
2022-12-10 19:13:18 +0700

    DAOProduct

--- src/main/java/com/dao/seller/impl/DAOProductSellerImpl.java
+++ src/main/java/com/dao/seller/impl/DAOProductSellerImpl.java
@@ -1,10 +1,13 @@
 package com.dao.seller.impl;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 
 import com.JPAConfig;
 import com.entity.Product;
+import com.entity.Store;
 
 public class DAOProductSellerImpl {
 	public Product insertProduct(Product product) {
@@ -13,6 +16,27 @@ public class DAOProductSellerImpl {
 		try {
 			transaction.begin();
 			entityManager.persist(product);
+			transaction.commit();
+			return product;
+		} catch (Exception e) {
+			e.printStackTrace();
+			transaction.rollback();
+			return null;
+		}
+	}
+	
+	public List<Product> getProductsByStoreId(Long storeId){
+		EntityManager entityManager = JPAConfig.getEntityManager();
+		Store store = entityManager.find(Store.class, storeId);
+		return store.getProducts();	
+	}
+	
+	public Product updateProduct(Product product) {
+		EntityManager entityManager = JPAConfig.getEntityManager();
+		EntityTransaction transaction = entityManager.getTransaction();
+		try {
+			transaction.begin();
+			entityManager.merge(product);
 			transaction.commit();
 			return product;
 		} catch (Exception e) {
